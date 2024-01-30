0b4ccca1cc7ea6800eb06514a9ac8d8b9975c31f
AimanFadil <aimanbello2001@gmail.com>
2024-01-30 14:32:52 +0100

    class

--- index.php
+++ index.php
@@ -6,15 +6,3 @@ class movie
     public $type;
     public $age;
 }
-
-$movie_1 = new movie();
-
-$movie_2 = new movie();
-
-$movie_3 = new movie();
-
-$movie_4 = new movie();
-
-$movie_5 = new movie();
-
-$movie_6 = new movie();
